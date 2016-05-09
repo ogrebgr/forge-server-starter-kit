78a0a2dd411ab1dc17b22ca38ec7afc14cf25783
Ognyan Bankov <obankov@objectsystems.com>
2016-05-09 17:51:08 +0300

    now logs the exception

--- src/main/java/com/bolyartech/forge/server/skeleton/misc/DbHandler.java
+++ src/main/java/com/bolyartech/forge/server/skeleton/misc/DbHandler.java
@@ -2,9 +2,9 @@ package com.bolyartech.forge.server.skeleton.misc;
 
 import com.bolyartech.forge.server.HandlerException;
 import com.bolyartech.forge.server.db.DbPool;
-import com.bolyartech.forge.server.misc.ForgeHandler;
 import com.bolyartech.forge.server.misc.ForgeResponse;
 import com.bolyartech.forge.server.misc.ForgeSecureHandler;
+import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 
@@ -13,6 +13,7 @@ import java.sql.SQLException;
 import java.text.MessageFormat;
 
 abstract public class DbHandler extends ForgeSecureHandler {
+    private final org.slf4j.Logger mLogger = LoggerFactory.getLogger(this.getClass().getSimpleName());
     private final DbPool mDbPool;
 
 
@@ -33,6 +34,7 @@ abstract public class DbHandler extends ForgeSecureHandler {
 
             return ret;
         } catch (SQLException e) {
+            mLogger.error("DB error {}", e);
             throw new HandlerException(MessageFormat.format("Error in handleSecure() with DB: {0}", e));
         }
     }

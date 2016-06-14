9aa43039b24b666f3b63bd3e121d6af3379c3b6d
Ognyan Bankov <ogibankov@gmail.com>
2016-06-14 09:03:38 +0300

    removing wrongly commited files

--- src/main/java/com/bolyartech/forge/server/modules/swoon/data/interaction/InteractionItemStaticText.java
+++
@@ -1,9 +0,0 @@
-package com.bolyartech.forge.server.modules.swoon.data.interaction;
-
-import com.bolyartech.forge.server.modules.swoon.data.interaction_item.InteractionItem;
-
-public interface InteractionItemStaticText {
-    long getId();
-    long getItem();
-    String getText();
-}
--- src/main/java/com/bolyartech/forge/server/modules/swoon/data/interaction/TextInteraction.java
+++
@@ -1,9 +0,0 @@
-package com.bolyartech.forge.server.modules.swoon.data.interaction;
-
-import com.bolyartech.forge.server.modules.swoon.data.interaction.Interaction;
-
-public class TextInteraction {
-    private Interaction mInteraction;
-    private
-
-}
